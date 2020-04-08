cedf5549e6acb5f6bcf043e8ef05ef5969a0a554
Brad Caron <bacaron@iu.edu>
2020-04-08 14:40:23 -0400

    initial commit. need to get fsleyes working on fsl dockers

--- run.sh
+++ run.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# t1/t2, flair, dwi, or fmri
-input=`jq -r '.input' config.json`
+# inputs
+anat=`jq -r '.anat' config.json`
+dwi=`jq -r '.dwi' config.json`
+bvals=`jq -r '.bvals' config.json`
 outdir="raw"
 
 mkdir -p $outdir
 
+# make brainmask
+select_dwi_vols ${dwi} ${bvals} nodif 0 -m
+bet nodif.nii.gz nodif_brain -f 0.4 -g 0 -m
+
+# render overlay
+fsleyes render -ds "world" -hc -hl -c 0 --outfile ${outdir}/out.png t1.nii.gz nodif_brain_mask.nii.gz -ot mask -o -mc 1 0 0
+
+
 slicer ${input} -a ${outdir}/out.png
 
-if [ ! -f ${outdir}/out.png ]; then
-	echo "failed"
-	exit 1
-else
-	echo "complete"
-	exit 0
-fi
+#if [ ! -f ${outdir}/out.png ]; then
+#	echo "failed"
+#	exit 1
+#else
+#	echo "complete"
+#	exit 0
+#fi
