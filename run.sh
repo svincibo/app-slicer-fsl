a795574e3842eb7fc7881052f74b25b106b718a7
Brad Caron <bacaron@iu.edu>
2020-05-11 12:08:57 -0400

    Reason: add volume input capability

--- run.sh
+++ run.sh
@@ -3,8 +3,14 @@
 # t1/t2, flair, dwi, or fmri
 input=`jq -r '.input' config.json`
 outdir="raw"
+vol=`jq -r '.volume' config.json`
 
 mkdir -p $outdir
+
+if [[ ! ${vol} == "" ]]; then
+	fslselectvols -i ${input} -o volume.nii.gz --vols=${vol}
+	input="volume.nii.gz"
+fi
 
 slicer ${input} -a ${outdir}/out.png
 
